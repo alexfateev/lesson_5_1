ea63d3a54c351f552760a601df8b7f15856c5edd
alexfateev <mailboxfav@gmail.com>
2024-10-21 10:16:40 +0500

    first commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿int simpleCount = 0;
+int simpleNumber = 2;
+int index = 2;
+int number;
+while (true)
+{
+    Console.Write("Укажите порядковый номер простого числа: ");
+    var res = int.TryParse(Console.ReadLine(), out number);
+    if (res)
+    {
+        while (simpleCount != number)
+        {
+            if (isSimple(index))
+            {
+                simpleCount++;
+                simpleNumber = index;
+            }
+            index++;
+        }
+        break;
+    }
+}
+
+
+Console.WriteLine($"{number} простое число - {simpleNumber}");
+
+
+bool isSimple(int number)
+{
+    var res = true;
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0) { res = false; break; }
+    }
+    return res;
+}
+
